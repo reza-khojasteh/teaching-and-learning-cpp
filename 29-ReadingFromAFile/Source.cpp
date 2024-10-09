d1bd2836a8be7aaee7d1a74dcbaded6d97542e7e
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2024-10-08 20:27:22 -0400

    Modify ReadFromAFile

--- 29-ReadingFromAFile/Source.cpp
+++ 29-ReadingFromAFile/Source.cpp
@@ -6,19 +6,27 @@
 
 int main() {
 	int i;
-
-	std::ifstream f("input.txt");
-	if (f.is_open()) {
-		while (f) {
-			f >> i;
-			if (f)
+	// Open file
+	std::ifstream fin("input.txt");
+	// Check if file is open
+	if (fin.is_open()) {
+		// As long as there is data in the file
+		// A stream object is not ready for further streaming if it has encountered an error and has not been cleared.
+		while (fin) {
+			// Read from file
+			fin >> i;
+			// Check if the read was successful
+			// If the read was successful, print the number
+			if (fin)
 				std::cout << i << ' ';
+			// If the read was not successful, print an error message and break the loop
 			else
-				std::cout << "\n**Bad input**\n";
+				std::cout << "\n**Bad input, reading from file failed and can no longer read from file**\n";
 		}
 	}
+	// If file is not open
 	else {
-		std::cout << "Error opening file\n";
+		std::cout << "Error opening file...\n";
 	}
 
 	return 0;
