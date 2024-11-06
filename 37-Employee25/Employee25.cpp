d62daf7bea31ee2952360d469697a234effb223c
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2024-11-05 21:49:43 -0500

    Add Employee 37

---
+++ 37-Employee25/Employee25.cpp
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+#include <cstring>
+
+#include "Employee25.h"
+
+namespace seneca {
+	Employee::Employee(long id, const char* fName, const char* lName) {
+		m_ID = id;
+		strcpy(m_fName, fName);
+		strcpy(m_lName, lName);
+	}
+
+	const Employee& Employee::print(ostream& os) const {
+		if (m_ID == 0 && m_fName[0] == '\0' && m_lName[0] == '\0')
+			os << endl << "Employee object is empty" << endl;
+		else {
+			os.setf(ios::fixed);
+			os.widen(10);
+
+			os << endl << "Employee ID: " << m_ID << endl
+				<< "Employee Name: " << m_fName << " " << m_lName << endl;
+
+			os.unsetf(ios::fixed);
+		}
+
+		return *this;
+	}
+
+	Employee& Employee::read(istream& is) {
+		long id{}; // will hold the employee ID
+		char fName[41]{}; // will hold the employee first name
+		char lName[41]{}; // will hold the employee last name
+
+		cout << "Enter Employee ID: ";
+		// read the ID from the input buffer
+		is >> m_ID;
+
+		cout << "Enter Employee First Name: ";
+		// read the first name from the input buffer
+		is >> m_fName;
+
+		cout << "Enter Employee Last Name: ";
+		// read the last name from the input buffer
+		is >> m_lName;
+
+		// construct a temporary Student
+		Employee temp(id, fName, lName);
+		// if data is valid, the temporary object into the current object
+		if (temp.m_ID != 0 && temp.m_fName[0] != '\0' && temp.m_lName[0] != '\0')
+			*this = temp;
+
+		return *this;
+	}
+
+	Employee::~Employee() {
+		// no dynamic memory to release
+	}
+
+	// This constructor calls the base class's constructor, 
+	// and then, does the initializations for the specific parts
+	HourlyBasedEmployee::HourlyBasedEmployee(long id, const char* fName, const char* lName, double noOfHoursWorked, double hourlyRate)
+		: Employee(id, fName, lName) {
+		setNoOfHorsWorked(noOfHoursWorked);
+		setHourlyRate(hourlyRate);
+	}
+
+	// And now, the implmentations for the derived class member functions:
+	double HourlyBasedEmployee::getNoOfHorsWorked() const {
+		return m_noOfHoursWorked;
+	}
+
+	double HourlyBasedEmployee::getHourlyRate() const {
+		return m_hourlyRate;
+	}
+
+	HourlyBasedEmployee& HourlyBasedEmployee::setNoOfHorsWorked(double noOfHoursWorked) {
+		m_noOfHoursWorked = noOfHoursWorked;
+		return *this;
+	}
+
+	HourlyBasedEmployee& HourlyBasedEmployee::setHourlyRate(double hourlyRate) {
+		m_hourlyRate = hourlyRate;
+		return *this;
+	}
+
+	const HourlyBasedEmployee& HourlyBasedEmployee::print(std::ostream& os) const {
+		Employee::print(os);
+
+		os << "Employee Number Of Hours Worked: " << m_noOfHoursWorked << endl
+			<< "Employee Hourly Rate: " << m_hourlyRate << endl;
+
+		return *this;
+	}
+}
---
+++ 37-Employee25/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+#include "Employee25.h"
+using namespace seneca;
+
+// A polymorphic function that can accept any object of the base class or its derived classes
+void display(const Employee& emp) {
+	emp.print(cout);
+}
+
+int main() {
+	// A polymorphic object that can change its dynamic type throughout its lifetime.
+	// It can be in any ways we can utilize an address of a base class object (like using a pointer or a reference)
+	// Let's start by creating a pointer to the base class
+	Employee* ptr = nullptr;
+
+	// This pointer can point to the base class object:
+	ptr = new Employee(1, "Jane", "Doe");
+	display(*ptr);
+	delete ptr;
+
+	// Later on, this pointer can point to the derived class object:
+	ptr = new HourlyBasedEmployee(2, "John", "Doe", 43.5, 50.0);
+	display(*ptr);
+	delete ptr;
+
+	return 0;
+}
