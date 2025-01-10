b7809df48d1371a97dd1f45e49d2bc97dcf4310c
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2025-01-09 22:06:48 -0500

    Turn line to comment

--- 2-intro-to-unit-tests/main.cpp
+++ 2-intro-to-unit-tests/main.cpp
@@ -11,7 +11,7 @@ int main() {
     passed += testSuite(5, 3, 125);
     passed += testSuite(5, -3, 0.008);
 	passed += testSuite(9, 0, 1);
-	passed += testSuite(0, 9, 0);
+	//passed += testSuite(0, 9, 0);
 
     cout << passed << " Tests Passed" << endl;
 }
