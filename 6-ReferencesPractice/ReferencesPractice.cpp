37138fa759d9ee2a3d028acb79f163b863049723
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2025-01-13 00:26:30 -0500

    Modify code in references practice

--- 6-ReferencesPractice/ReferencesPractice.cpp
+++ 6-ReferencesPractice/ReferencesPractice.cpp
@@ -7,18 +7,18 @@ void foo(int& x) {
 	cout << x << endl;
 
 	// This line increments the value of the integer that is one memory address away from the reference.
-	*(&x + 1) = 4;
+	*(&x + 1) += 1; // OR (*(&x + 1))++;
 }
 
 int main() {
-	// This line creates an array of 3 integers and initializes the first three elements to 2.
+	// This line creates an array of 3 integers and initializes it with the values 1, 2, and 3.
 	int a[3]{ 1, 2, 3 };
 
 	// This line prints out the first three elements of the array, initially.
 	cout << a[0] << a[1] << a[2] << endl;
 
 	// This line passes the dereference of the first element of the array to the foo function.
-	// (Dereferencing is the act of accessing the value stored at the memory address pointed to by a pointer.
+	// (Dereferencing is the act of accessing the value stored at the memory address pointed to by a pointer.)
 	foo(*a);
 
 	// This line prints out the first three elements of the array, again!
