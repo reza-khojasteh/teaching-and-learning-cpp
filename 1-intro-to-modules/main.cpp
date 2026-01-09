776c23ffa3a08d0db36ade750bffb87defb7afea
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2026-01-09 11:17:32 -0500

    Add a comment to main.cpp in the first solution/folder

--- 1-intro-to-modules/main.cpp
+++ 1-intro-to-modules/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
     int i;
-    Transaction tr;
+	Transaction tr;// Or struct Transaction tr; as you did in C
 	// loop to enter and display transactions
     for (i = 0; i < NO_TRANSACTIONS; i++) {
         enter(&tr);
