4a4a18599e4dbf342677088c6563927009bfe239
reza-khojasteh <mohammad.reza.khojasteh@gmail.com>
2026-01-09 11:27:04 -0500

    Add comments to main.cpp in the second solution/folder

--- 2-intro-to-unit-tests/main.cpp
+++ 2-intro-to-unit-tests/main.cpp
@@ -9,8 +9,9 @@ int main() {
     int passed = 0;
 
     passed += testSuite(5, 3, 125);
-    passed += testSuite(5, -3, 0.008);
-	passed += testSuite(9, 0, 1);
+	passed += testSuite(5, -3, 0.008);// We have now solved the issue we had with the version in the notes! (check Calculator.cpp)
+	// But what about the other test cases? (check the ones commented out below)
+	//passed += testSuite(9, 0, 1); 
 	//passed += testSuite(0, 9, 0);
 
     cout << passed << " Tests Passed" << endl;
